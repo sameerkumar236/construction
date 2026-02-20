e5742f6b986bc87867841dcb0a88ba4aa353a198
Sameer Kumar <sameersizoxta625@gmail.com>
2026-02-20 14:00:49 +0530

    Initial commit

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "./section/Hero";   // agar Hero.jsx src ke andar hai
+
+function App() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
+
+export default App;
